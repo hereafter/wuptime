38a284694f08bfd58ea5b9fbce8894ebc85ee2c1
hereafter <tu.tong@outlook.com>
2022-10-24 00:36:44 +0800

    remove cppwinrt

--- src/wuptime/main.cpp
+++ src/wuptime/main.cpp
@@ -1,10 +1,6 @@
 ﻿#include "pch.h"
 
-using namespace winrt;
-using namespace Windows::Foundation;
-
 int main()
 {
-    init_apartment();
     return 0;
 }
--- src/wuptime/pch.cpp
+++ src/wuptime/pch.cpp
@@ -1,3 +1,1 @@
 ﻿#include "pch.h"
-
-#pragma comment(lib, "WindowsApp.lib")
