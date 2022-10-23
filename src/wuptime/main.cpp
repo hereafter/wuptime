c6ab57536b408538907907023462bd3b0d4563e3
hereafter <tu.tong@outlook.com>
2022-10-24 01:15:06 +0800

    ticks

--- src/wuptime/main.cpp
+++ src/wuptime/main.cpp
@@ -1,6 +1,17 @@
 ﻿#include "pch.h"
 
+int64_t get_uptime();
+
+
 int main()
 {
+    auto times = get_uptime();
+    return 0;
+}
+
+
+int64_t get_uptime()
+{
+    auto ticks=GetTickCount64();
     return 0;
 }
--- src/wuptime/pch.h
+++ src/wuptime/pch.h
@@ -1,1 +1,8 @@
 ﻿#pragma once
+#define WIN32_LEAN_AND_MEAN
+
+#include <cstdint>
+#include <sstream>
+#include <string>
+#include <tchar.h>
+#include <Windows.h>
